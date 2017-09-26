ee835a9b14df9916863581996de74c903d5514c2
Alexander Kavon <hawk@alexkavon.com>
2017-09-25 22:12:01 -0600

    storage init in preparation for storage interface and plugin system

--- store/store.go
+++ store/store.go
@@ -1,5 +1,20 @@
 package store
 
 import (
+	"github.com/boltdb/bolt"
 	"gitlab.com/alexkavon/loaf/store"
 )
+
+type Store struct {
+	Name string
+	Host string
+	Port int64
+}
+
+func NewStore() Store {
+	db, err := bolt.Open("my.db", 0600, nil)
+	if err != nil {
+		return
+	}
+	defer db.Close()
+}
